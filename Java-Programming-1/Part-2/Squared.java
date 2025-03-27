63041647bf76c98c0d13888e5ead6e75239abd0f
Marina <mtsiatiri@gmail.com>
2025-03-27 10:18:12 +0200

    Create Squared.java

---
+++ Java-Programming-1/Part-2/Squared.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int num = Integer.valueOf(scan.nextLine());
+        System.out.println(num * num);
+
+    }
+}
